f168aedcf1a3099ad0400af48c9052c806b2c9d2
Sasseke <sasseku@gmail.com>
2023-10-02 13:01:34 +0200

    bestand tijdelijk.py gecodeerd

--- tijdelijk.py
+++ tijdelijk.py
@@ -7,5 +7,18 @@ prijzen = {
 aanbieding = prijzen["vanille"] * 0.8
 
 reclame_tekst = (f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}")
+reclame_tekst2 = reclame_tekst[:63]
+reclame_tekst3 = reclame_tekst2.upper()
+reclame_tekst4 = reclame_tekst3.split(" ")
 
-print(reclame_tekst)
+for el in reclame_tekst4:
+ print(el)
+
+for el in reclame_tekst4:
+ print(el.lower())
+
+for el in reclame_tekst4:
+ if(len(el) > 4):
+   print(el.upper())
+ else:
+   print(el.lower())
