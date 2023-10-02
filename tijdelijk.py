f7e8a60c5e2676af390963af1d8c9eb8c20e6cf1
Sasseke <sasseku@gmail.com>
2023-10-02 12:58:45 +0200

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,11 @@
+prijzen = {
+    "aardbei" : 3,
+    "vanille" : 4,
+    "chocolade" : 5
+}
+
+aanbieding = prijzen["vanille"] * 0.8
+
+reclame_tekst = (f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding}")
+
+print(reclame_tekst)
